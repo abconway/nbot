f63f9627a7181db6bf09188bb68852e916a3e9a8
Alex Conway <abconway2@gmail.com>
2023-06-23 15:13:13 -0400

    fix vitality

--- ui/vitality.py
+++ ui/vitality.py
@@ -5,6 +5,5 @@ import pywinauto
 
 def vitality(dlg):
     dlg.set_focus()
-    sleep(0.3)
-    pywinauto.keyboard.send_keys('{3 down}' '{3 up}')
     sleep(4)
+    pywinauto.keyboard.send_keys('{3 down}' '{3 up}')
